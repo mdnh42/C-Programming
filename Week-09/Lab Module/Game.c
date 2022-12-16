06f976edcbab258474049bf1070941a01cb0f7bf
itzmrnh <mdnh.42@gmail.com>
2022-12-16 11:39:44 +0600

    Tic Tac Toe

--- Week-09/Lab Module/Game.c
+++ Week-09/Lab Module/Game.c
@@ -1,79 +1,14 @@
-/*
-1. Array[3][4] = -1
-2. Player1(X) and Player2(O)
-2. Player1 r, c = 1 = X and Player2 r, c = 2 = O
-
-
-*/
-
 #include<stdio.h>
 #include<stdbool.h>
-// Who wiwn
-int is_win(int a[][4], int n)
-{
-    for(int i=1; i<=n; i++)
-    {
-        if(a[i][1]=a[i][2] && a[i][2] == a[i][3] && a[i][1] != -1)
-            return a[i][1];
-    }
-    for(int j=1; j<=n; j++)
-    {
-        if(a[1][j]=a[2][j] && a[2][j] == a[3][j] && a[1][j] != -1)
-            return a[1][j];
-    }
-
-    if(a[1][1] == a[2][2] && a[2][2]==a[3][3] &&a[1][1] != -1)
-        return a[1][1];
-
-    if(a[1][3] == a[2][2] && a[2][2]==a[3][1] && a[1][3] != -1)
-        return a[1][3];
-}
-// Tic-Tac Box Printng Function
-void printCell(int a[4][4], int n)
-{
-    for(int i=1; i<=n; i++)
-    {
-        for(int j=1; j<=n; j++)
-        {
-            if(a[i][j] == -1) printf(" ");
-            if(a[i][j] == 1) printf("X");
-            if(a[i][j] == 2) printf("O");
-            if(j<n) printf("\t|\t");
-        }
-        if(i<n) printf("\n______________________________________\n");
-        printf("\n\n");
-
-    }
-
-}
-void printWin(int a[4][4], int n, int who)
-{
-    for(int i=1; i<=n; i++)
-    {
-        for(int j=1; j<=n; j++)
-        {
-            if(a[i][j] == who)
-            {
-                if(a[i][j] == 1) printf("X");
-                else printf("O");
-            }
-            else
-            {
-                printf(" ");
-            }
-            if(j<n) printf("\t|\t");
-        }
-        if(i<n) printf("\n______________________________________\n");
-        printf("\n\n");
-
-    }
-
-}
+int isWin(int a[][4], int n);
+void printCell(int a[][4], int n);
+//Father of Funciton
 int main()
 {
-    int n= 3;
+    //Decalare a variable how much value and 2D Array for 4 Value !0, 1 2 3
+    int n = 3;
     int a[4][4];
-    // Set Every Postion -1
+    // Set Every postion - 1
     for(int i=1; i<=n; i++)
     {
         for(int j=1; j<=n; j++)
@@ -81,53 +16,104 @@ int main()
             a[i][j] = -1;
         }
     }
-    bool player1 = true, player2= false;
+    // Input from Salma and Rashal
+    // check you input and not allow same input more one times.
+    bool salma = true;
+    bool rashal = false;
     while(true)
     {
         printCell(a, n);
-        if(player1)
+        if(salma == true)
         {
             int r, c;
-            Flag:
-            printf("Player 1 Trun (X), Enter Row and Column: ");
-            scanf("%d %d", &r, &c);
+            SALMAGF:
+            printf("SALMA Trun (X), Enter Row and Column: ");
+            scanf("%d%d",&r, &c);
             if(a[r][c] != -1)
             {
-                printf("Invalid Cell\n");
-                goto Flag;
+                printf("Invalid Cell\n ");
+                goto SALMAGF;
             }
             a[r][c] = 1;
-            player1 = false;
-            player2 = true;
+            salma = false;
+            rashal = true;
         }
         else
         {
             int r, c;
-            Flag2:
-            printf("Player 2 Trun (X), Enter Row and Column: ");
-            scanf("%d %d", &r, &c);
+            RASHALBF:
+            printf("RASHAL Trun (O), Enter Row and Column: ");
+            scanf("%d%d",&r, &c);
             if(a[r][c] != -1)
             {
-                printf("Invalid Cell\n");
-                goto Flag2;
+                printf("Invalid Cell\n ");
+                goto RASHALBF;
             }
             a[r][c] = 2;
-            player2 = false;
-            player1=true;
+            rashal = false;
+            salma = true;
         }
-//        check who win
-        if(is_win(a, n)==1)
+        //Check Who Win?
+        if(isWin(a,n)==1)
         {
-            printf("Plalyer 1 Won!\n");
-            printWin(a,n, 1);
+            printf("Salma Won!\n");
+            printCell(a,n);
             break;
         }
-        else if(is_win(a, n) == 2)
+        else if(isWin(a,n)==2)
         {
-            printf("Player 2 Won!\n");
-            printWin(a,n, 2);
+            printf("Rashal Won!\n");
+            printCell(a, n);
             break;
+        }
+
+    }
+
+
+    return 0;
+}
+//print the Cell Function for X, O positoing design
+void printCell(int a[][4], int n)
+{
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            if(a[i][j] == -1) printf(" ");
+            if(a[i][j]==1) printf("X");
+            if(a[i][j]==2) printf("O");
+            if(j<n) printf("\t|\t");
+        }
+        printf("\n");
+        if(i<n) printf("_______________________________");
+        printf("\n");
+    }
+
+}
+//Is Win
+int isWin(int a[][4], int n)
+{
+    for(int i=1; i<=n; i++)
+    {
+        if(a[i][1] == a[i][2] && a[i][2] == a[i][3] && a[i][1] != -1)
+        {
+            return a[i][1];
         }
     }
-    return 0;
+    for(int j=1; j<=n; j++)
+    {
+        if(a[1][j] == a[2][j] && a[2][j] == a[3][j] && a[1][j] != -1)
+        {
+            return a[1][j];
+        }
+    }
+    if(a[1][1] == a[2][2] && a[2][2] == a[3][3] && a[1][1] != -1)
+    {
+        return a[1][1];
+    }
+    if(a[1][3] == a[2][2] && a[3][1] == a[3][1] && a[1][3] != -1)
+    {
+        return a[1][3];
+    }
+    return -1;
 }
---
+++ Week-09/Lab Module/ReadMe.md
@@ -0,0 +1,60 @@
+Tic Tac Toe Game in C Programming  
+For Rashal AND SALMA 
+Naive THinking-
+Salma (X)
+RASHAL (O)
+        |               |
+_______________________________
+        |               |
+_______________________________
+        |               |
+
+SALMA Trun (X), Enter Row and Column: 1 1
+X       |               |
+_______________________________
+        |               |
+_______________________________
+        |               |
+
+RASHAL Trun (O), Enter Row and Column: 1 2
+X       |       O       |
+_______________________________
+        |               |
+_______________________________
+        |               |
+
+SALMA Trun (X), Enter Row and Column: 2 1
+X       |       O       |
+_______________________________
+X       |               |
+_______________________________
+        |               |
+
+RASHAL Trun (O), Enter Row and Column: 3 2
+X       |       O       |
+_______________________________
+X       |               |
+_______________________________
+        |       O       |
+
+SALMA Trun (X), Enter Row and Column: 3 1
+Salma Won!
+X       |       O       |
+_______________________________
+X       |               |
+_______________________________
+X       |       O       |
+
+
+Step 1: Declare Row and Column. 
+Step 2: 2D Array Index -1 
+Step 3: Input from Salma row and Column (X) and Rashal row and Column (O)
+Step 4: Salma -1 == 1 == 'X' and Rashal -1 == 2 == 'O'
+Step 5: Check Who Win? 
+        i && j[1, 2, 3] ___________ 
+        [1, 2, 3], j     |
+        [1, 1 && 2, 2 && 3, 3] 
+        1, 3 && 2, 2 && 1, 3
+
+
+Updating.................
