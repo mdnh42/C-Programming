48bb5378691cb4d12d30f9b5eb1eb11be6b85280
itzmrnh <mdnh.42@gmail.com>
2022-12-17 22:58:19 +0600

    Update

--- Data Structure and Algorithm Tamim/stack.c
+++
@@ -1,9 +0,0 @@
-
-#define STACk_MAX 100
-
-typedef struct{
-
-    int top;
-    int data[STACk_MAX];
-
-}stack;
--- VS C Programming Test/test.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    int n=5;
-    
-    printf("Square: %d",n*n);
-    return 0;
-}
